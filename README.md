723219cffe832b40294187ee5f2d68537d23cde6
Adam DiLosa <adam@dilosa.com>
2014-05-14 20:53:57 -0400

    Rebuilt using bootstrap.

--- README.md
+++
@@ -1,5 +0,0 @@
-tv
-==
-
-Aggregates links for fast access to online TV shows.
-
--- pages/music/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-	<head>
-		<link rel="stylesheet" href="../../css/normalize.min.css">
-        <link rel="stylesheet" href="../../css/main.css">
-	</head>
-	<body>
-		<?php include '../../header.php'; ?>
-	</body>
-</html>
